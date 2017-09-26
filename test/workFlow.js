cf065ad243252bfac4488788dec018eff1e2e72c
Hien Tran <aznketchup@yahoo.com>
2017-09-25 20:10:33 -0700

    Installed pdfjs-dist to convert pdf to json object. -Included pdfjs-dist in workFlow test suite to test the pdf made on the server.

--- test/workFlow.js
+++ test/workFlow.js
@@ -1,15 +1,58 @@
-var config = require('../nightwatch.config.js');
-var fs = require('fs');
+let config = require('../nightwatch.config.js');
+let fs = require('fs');
+let PDFJS = require('pdfjs-dist');
 
 module.exports = {
-	before: function(browser) {
-		fs.unlink('./test.pdf');
-	},
+	// before: function(browser) {
+	// 	if (fs.existsSync('./test.pdf')) {
+	// 		fs.unlink('./test.pdf');
+	// 	}
+	// },
 	'Check for button': function(browser) {
 		browser
 			.url('localhost:3005')
 			.waitForElementVisible('body')
 			.assert.elementPresent('button.btn.btn-secondary')
-			.end();
+			.click('button.btn.btn-secondary');
+	},
+	'Look at pdf': function(browser) {
+		let data = new Uint8Array(fs.readFileSync('./test.pdf'));
+		PDFJS.getDocument(data).then(function(pdfDocument) {
+			pdfDocument.getPage(1).then(function(page) {
+				page.getTextContent().then(function(content) {
+					console.log(content.items[0].transform);
+					let list = content.items.map(function(value) {
+						return value.str;
+					});
+					let structuredList = {};
+					let getHeader = function(list) {
+						let header = {};
+						let lastHeaderWords = 'Invoice Date';
+						let runDate = 'Run Date';
+						let title = 'Daily Invoice Report';
+						let condition = true;
+
+						while (condition) {
+							if (list[0].includes(runDate)) {
+								header.runDate = list[0].split(' ')[2];
+							} else if (list[0].includes(title)) {
+								header.title = list[0];
+							} else if (list[0].includes(lastHeaderWords)) {
+								header.invoiceDate = list[0].split(' ')[2];
+								condition = false;
+							} else {
+								header.brokerName = list[0];
+							}
+							list.shift();
+						}
+
+						return header;
+					};
+					console.log(getHeader(list));
+					console.log(list);
+				});
+			});
+		});
+		browser.end();
 	}
 };
